126800e2e39ed3ae28fdbfafa1819ece2bdaeb8f
LightningStairs <jenah.blitz@icloud.com>
2018-07-15 14:20:12 -0400

    update import to correct syntax

--- src/game.js
+++ src/game.js
@@ -8,7 +8,7 @@
 // game.playMove(1, 2);
 // When done run `.exit`
 
-import { Board } from './board.js'
+import { Board } from './board';
 
 class Game {
   constructor(numberOfRows, numberOfColumns, numberOfBombs) {
